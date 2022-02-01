a54e543eaacbf9fecd2d797e85c24839e7cc84ae
Daniel Brauner <daniel-brauner@outlook.com>
2022-02-01 13:17:21 +0100

    [add] field mapping test

--- lib/formz_test.dart
+++ lib/formz_test.dart
@@ -9,6 +9,6 @@ export 'src/generic_test_input.dart';
 export 'src/input_matcher.dart';
 export 'src/input_test_utils.dart';
 export 'src/local_test_utils.dart';
-export 'src/parser_test.dart';
+export 'src/parsing/field_mapping_test.dart' show FieldMapping, fieldMappingTest;
 export 'src/select_matcher.dart';
 export 'src/test_utils.dart';
